a277572532a99d7fc97e67dcea17b384f2c106cb
Varga Márton <martonvrg92@gmail.com>
2022-09-15 19:06:42 +0200

    wrote missing commands for dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 import express from 'express';
 import routes from './src/rest/routes';
-const app = express();
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json())
 
-app.listen(3001, () => {
-  console.log("server started")
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
 })
 
 app.use(routes);
