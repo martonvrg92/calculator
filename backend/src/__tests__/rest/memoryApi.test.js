e07b8175e0879b6e7a373821a45eb53adffd34a6
Varga Márton <martonvrg92@gmail.com>
2022-09-19 12:33:01 +0200

    Added unit test for post endpoint

---
+++ backend/src/__tests__/rest/memoryApi.test.js
@@ -0,0 +1,15 @@
+import supertest from 'supertest';
+import app from '../../rest'
+import { memoryService } from '../../services';
+
+describe('POST memory/save', () => {
+  const server = supertest(app);
+  it('should response with status code 200',async ()=> {
+    const requestBody = {
+      data: await memoryService.memoryRecall('/file.txt')
+    };
+    const res = await server.post('/memory/save').send(requestBody);
+    expect(res.status).toEqual(200);
+    expect(res.body.message).toBe('saved to memory');
+  })
+})
