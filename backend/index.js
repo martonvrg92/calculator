4b024c0e443689d0d97a51198da7ddd0ca0fc8c6
Varga Márton <martonvrg92@gmail.com>
2022-09-19 12:34:51 +0200

    changed structure of backend(server start)

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,9 @@
-import express from 'express';
-import routes from './src/rest/routes';
-import cors from 'cors';
 import dotenv from 'dotenv';
+import server from './src/rest'
 
 dotenv.config();
-const app = express();
+const PORT = process.env.PORT
 
-const PORT = process.env.PORT;
-
-app.use(cors());
-app.use(express.json())
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
-
-app.use(routes);
+server.listen(PORT, ()=> {
+  console.log(`Server started on port:${PORT}`)
+});
---
+++ backend/src/rest/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import http from 'http';
+const app = express();
+const httpServer = http.createServer(app)
+
+app.use(cors());
+app.use(express.json())
+app.use(routes);
+
+export default httpServer;
